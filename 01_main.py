6022d1f62b5e526bba6a28a4c6ed11b34082b4b6
Gautam Gupta <guptagautam908@gmail.com>
2025-08-10 05:06:49 +0530

    Phase-4 --> Password Generation + printing the password (if part completed)

--- 01_main.py
+++ 01_main.py
@@ -4,13 +4,14 @@ import string
 import sys
 import pyperclip
 
+
 # Phase-2 --> Taking Input from the user
 while True:
     try:
         print("\nWelcome to the Random Password Generator!\n")
 
         length = int(input("Enter the length of the password(in integers) which you want to generate : "))
-        print("\nWhat things you want to include in your password❓\n")
+        print("What things you want to include in your password❓\n")
         include_numbers = str(input("Do you want to include Numbers ?(Y/N): ")).lower().strip() in ['y', 'yes']
         include_lowercase = str(input("Do you want to include Lowercase characters ?(Y/N): ")).lower().strip() in ['y', 'yes']
         include_uppercase = str(input("Do you want to include Uppercase characters ?(Y/N): ")).lower().strip() in ['y', 'yes']
@@ -18,6 +19,7 @@ while True:
         break
     except ValueError:
         print("\nPlease Enter a valid integer!")
+
 
 # Phase-3 --> Construction of Character Pool
 char_pool = ""
@@ -29,5 +31,58 @@ if(include_uppercase):
     char_pool += string.ascii_uppercase
 if(include_punctuations):
     char_pool += string.punctuation
-print(char_pool)
+# print(char_pool)
+if char_pool == "":
+    print("Please Select atleast one choice from the provided options")
+    sys.exit()
 
+
+# Phase-4 Password Generation + Printing the password
+resp = str(input("\nDo you want to include one character from each of your choice? (Y/N): ")).strip().lower()
+
+if resp not in ['y','yes', 'n', 'no']:
+    print("Please Enter a Vaild Choice!")
+    sys.exit()
+
+enforce = resp in ['y','yes']
+if (enforce):
+    guaranteed_char = []
+    if(include_numbers):
+        guaranteed_char.append(random.choice(string.digits))
+    if(include_lowercase):
+        guaranteed_char.append(random.choice(string.ascii_lowercase))
+    if(include_uppercase):
+        guaranteed_char.append(random.choice(string.ascii_uppercase))
+    if(include_punctuations):
+        guaranteed_char.append(random.choice(string.punctuation))
+    # print(guaranteed_char)
+
+
+     # finding lenght of the remaining password
+    random_chars = []
+    required_length= length-len(guaranteed_char)
+
+
+     # Selecting remainging password
+    for i in range(required_length):
+        random_chars.append(random.choice(char_pool))
+    
+    # Merging the both the lists
+    password_char = guaranteed_char + random_chars
+    print(password_char)
+
+
+    # Shuffling the list
+    random.shuffle(password_char)
+    
+    # Converting List into string
+    final_password = "".join(password_char)
+
+
+    # copying the password into your clipboard
+    pyperclip.copy(final_password)
+    print(f"Your Password is : {final_password}")
+    print("The password is copied in your clipboard automatically ")
+else:
+    
+    
